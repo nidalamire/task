ea04e292dc869b1aca1b90ee12ee8a82c4191e75
nidalamire <91350499+nidalamire@users.noreply.github.com>
2021-10-19 23:10:35 +0300

    Add files via upload

---
+++ nid/Untitled-1.js
@@ -0,0 +1,8 @@
+
+function but(){
+    var image=document.createElement('img');
+    var div= document.getElementById('flex-cat-gen');
+    image.scr="https://images.search.yahoo.com/search/images;_ylt=AwrJ61ULFW9hWOUA295XNyoA;_ylu=Y29sbwNiZjEEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=img&fr2=piv-web&type=E210US91215G0&fr=mcafee#id=1&iurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2014%2F12%2F28%2F13%2F20%2Fwordpress-581849_960_720.jpg&action=click.png";
+    div.appendChild(image);
+
+}  
